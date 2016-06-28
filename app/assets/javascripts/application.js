9682c585dd0fc0359f971b04a3545da2097762b3
Matt <matthewmorgante@gmail.com>
2016-06-28 16:28:37 +0200

    custom params hash

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,7 +15,6 @@
 //= require turbolinks
 //= require_tree .
 
-
 $(function(){
   document.getElementById('user');
 })
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,22 +4,12 @@ class UsersController < ApplicationController
   end   
 
   def set_keys
-    @consumer_key = "springest_-_testklant.1"
-    @consumer_secret = "zvhFujzLrr"
-    @launch_url = "https://apps.goodhabitz.com/lti"
+    @consumer_key = Rails.application.secrets.good_habitz_key
+    @consumer_secret = Rails.application.secrets.good_habitz_secret
   end 
 
-  def has_required_params? 
-    @consumer_key && @consumer_secret && @launch_url && @resource_link_id
-  end 
-
-  def create 
-    # raise "Don't have all your require params" unless has_required_params?
-
-    # should have lti parameters 
-    @lti_message_type = 'basic-lti-launch-request'
-    @lti_version = 'LTI-1p0'
-
+  def create
+    @user = User.new(user_params)
     # uri = URI.parse(@launch_url)
     # unique_slug = "/SLM_13592_O_Content/1"
     # host = uri + unique_slug
@@ -27,29 +17,37 @@ class UsersController < ApplicationController
     path = "https://apps.goodhabitz.com/lti/SLM_13592_O_Content/1"
 
     consumer = OAuth::Consumer.new(@consumer_key, @consumer_secret, { 
-      :site => "https://apps.goodhabitz.com/lti/SLM_13592_O_Content/1", 
+      :site => path, 
       :signature_method => "HMAC-SHA1",
+      :oauth_callback => "about:blank",
        })
 
     options = { 
       :scheme => 'body'
     }
-
-    params['lti_version'] ||= "LTI-1p0"
-
+    
+    @custom_params = {}
+    @custom_params[:lti_version] = "LTI-1p0"
+    @custom_params[:lti_message_type] = "basic-lti-launch-request"
+    @custom_params[:resource_link_id] = "randomstring88391-e1919-bb3456"
+    @custom_params[:user_id] = @user.user_id
+ 
     request = consumer.create_signed_request(:post, path, nil, options, params)
-
       # the request is made by a html form in the user's browser, so we
       # want to revert the escapage and return the hash of post parameters ready
       # for embedding in a html view
     hash = {}
     request.body.split(/&/).each do |param|
-      key, val = param.split('/=/').map { |v| CGI.unescape(v) }
+      key, val = param.split('=').map { |v| CGI.unescape(v) }
       hash[key] = val
     end
-    puts hash
 
     redirect_to users_path
+    puts "==================="
+    puts @custom_params
+    puts "==================="
+    puts hash 
+    puts "==================="
   end 
 
   def oauth_callback_url
